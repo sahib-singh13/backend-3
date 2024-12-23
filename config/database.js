40c323868b1e652df63eea6946c36a81aa43be87
sahib-singh13 <devsahibsingh@gmail.com>
2024-12-23 20:23:29 +0530

    done basic work

---
+++ config/database.js
@@ -0,0 +1,27 @@
+const mongoose =require("mongoose");
+
+/*
+  with this command below whathever i have 
+  written in env command would stored in the
+  process command
+*/
+
+require("dotenv").config();
+
+const dbConnect = () =>{
+    /*
+    here the process stores the .env command
+    by using the npm i dotenv which loads the
+    env file in the processes folder
+     */
+
+    mongoose.connect(process.env.DATABASE_URL)
+    .then(()=>console.log("DB connection successful"))
+    .catch((error)=>{
+        console.log("Issue in connection");
+        console.log(error.message);
+        process.exit(1);
+
+    });
+}
+module.exports=dbConnect;
